7929b0162383642290642083a55e4be7770f397f
Lee <516127941@qq.com>
2022-08-09 19:31:05 +0800

    test: pre-commit

--- ede.js
+++ ede.js
@@ -37,6 +37,7 @@
                 this.is_new_video = true
             }
         }
+
         /* https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js */
         /*! jQuery v1.12.4 | (c) jQuery Foundation | jquery.org/license */
         !function (a, b) { "object" == typeof module && "object" == typeof module.exports ? module.exports = a.document ? b(a, !0) : function (a) { if (!a.document) throw new Error("jQuery requires a window with a document"); return b(a) } : b(a) }("undefined" != typeof window ? window : this, function (a, b) {
@@ -54,6 +55,8 @@
                 }, function (c, d) { n.fn[d] = function (d, e) { var f = arguments.length && (c || "boolean" != typeof d), g = c || (d === !0 || e === !0 ? "margin" : "border"); return Y(this, function (b, c, d) { var e; return n.isWindow(b) ? b.document.documentElement["client" + a] : 9 === b.nodeType ? (e = b.documentElement, Math.max(b.body["scroll" + a], e["scroll" + a], b.body["offset" + a], e["offset" + a], e["client" + a])) : void 0 === d ? n.css(b, c, g) : n.style(b, c, d, g) }, b, f ? d : void 0, f, null) } })
             }), n.fn.extend({ bind: function (a, b, c) { return this.on(a, null, b, c) }, unbind: function (a, b) { return this.off(a, null, b) }, delegate: function (a, b, c, d) { return this.on(b, a, c, d) }, undelegate: function (a, b, c) { return 1 === arguments.length ? this.off(a, "**") : this.off(b, a || "**", c) } }), n.fn.size = function () { return this.length }, n.fn.andSelf = n.fn.addBack, "function" == typeof define && define.amd && define("jquery", [], function () { return n }); var nc = a.jQuery, oc = a.$; return n.noConflict = function (b) { return a.$ === n && (a.$ = oc), b && a.jQuery === n && (a.jQuery = nc), n }, b || (a.jQuery = a.$ = n), n
         });
+
+        /* 排除token冲突 */
         var _$ = $.noConflict();
         /* jQuery plugin: wait  */
         _$.fn.wait = function (func, times, interval) {
